d71172a8d5cfb50e3afa607b0ea67947bfcea6b3
Igor Maravic <igor@spotify.com>
2014-08-08 15:30:04 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Igor Maravić
+
+import setuptools
+
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
---
+++ test/puppetdb_api_test.py
@@ -0,0 +1,7 @@
+import unittest2
+
+
+class PuppetDBApiTest(unittest2.TestCase):
+
+    def test_placeholder(self):
+        print 'Hello'
